e90c7b3f5bcc66a4390c5311e154cf053a7caf26
Omkar Pisal <omkarpisal@Omkars-MacBook-Air.local>
2022-11-26 20:38:21 -0700

    Added Nimbus and Feel for better UI and Resolved Initial screen menu not displaying issue

---
+++ CSE563_Final_Project/MainFrame.java
@@ -0,0 +1,40 @@
+package CSE563_Final_Project;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame {
+    private JFrame frame;
+    private JPanel panel;
+
+    public MainFrame() {
+        initialize();
+    }
+
+    private void initialize() {
+
+        frame = new JFrame();
+        frame.setTitle("CSE563 Final Project");
+
+        panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
+
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(800, 500);
+        frame.setLocationRelativeTo(null);
+
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+
+        JMenuBar menuBarMain = new JMenuBar();
+        frame.setJMenuBar(menuBarMain);
+
+        menuBarMain.add(new File());
+
+        JMenu optionAbout = new JMenu();
+        menuBarMain.add(new About(optionAbout));
+
+
+    }
+}
